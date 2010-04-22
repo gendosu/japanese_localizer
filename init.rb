559b52e50f6b93cdc8324d829d61343146934887
root <root@centos54.(none)>
2010-04-22 22:00:59 +0900

    2.3.4ではconfig/localeをload_pathに追加してないが、2.3.5ではRailsがload_pathに追加しているため、その対応

--- init.rb
+++ init.rb
@@ -9,5 +9,14 @@ require File.dirname(__FILE__) + '/lib/japanese_localizer'
 
 I18n.default_locale = :jp
 I18n.locale = :jp
-I18n.load_path.concat(Dir[File.join(File.dirname(__FILE__), 'locale', '*.{rb,yml}')])
-I18n.load_path.concat(Dir[File.join(RAILS_ROOT, 'config', 'locale', '*.{rb,yml}')])
+
+plugin_locale  = Dir[File.join(File.dirname(__FILE__), 'locale', '*.{rb,yml}')]
+project_locale = Dir[File.join(RAILS_ROOT, 'config', 'locale', '*.{rb,yml}')]
+
+if Rails::VERSION::MAJOR == 2 && Rails::VERSION::MINOR == 3 && Rails::VERSION::TINY >= 5
+  I18n.load_path.insert(I18n.load_path.count - project_locale.count, plugin_locale)
+else
+  I18n.load_path.concat(plugin_locale)
+  I18n.load_path.concat(project_locale)
+end
+
